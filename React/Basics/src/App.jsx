c05a0fea498d8879eea89d358892cf51f188de4e
Akhil Rana <akhilrana4104@gmail.com>
2026-04-17 22:07:25 +0530

    Add

--- React/Basics/src/App.jsx
+++ React/Basics/src/App.jsx
@@ -97,25 +97,36 @@
 // export default App;
 
 
-import React, {useState, useEffect } from 'react'
+// import React, {useState, useEffect } from 'react'
 
-const App = () => {
-    const [count, setCount] = useState(0);
-    useEffect(() => {
-        console.log("Component mounted");
-    }, [count]);
-    console.log("Component rendered");
+// const App = () => {
+//     const [count, setCount] = useState(0);
+//     useEffect(() => {
+//         console.log("Component mounted");
+//     }, [count]);
+//     console.log("Component rendered");
    
 
-    let handleCount = () => {
-        setCount(prev => prev + 1);
-    }
+//     let handleCount = () => {
+//         setCount(prev => prev + 1);
+//     }
 
+//   return (
+//       <div>
+//           <h1>Count: {count}</h1>
+//           <button onClick={handleCount}>Increment</button>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+import React from 'react'
+import Slider from './Day2/Task/Slider';
+
+const App = () => {
   return (
-      <div>
-          <h1>Count: {count}</h1>
-          <button onClick={handleCount}>Increment</button>
-    </div>
+    <Slider/>
   )
 }
 
